503e73a204a3e2d475e2c47612ba33c342a9603b
Peter Sagerson <psagers.hg@ignorare.net>
2011-03-27 12:59:45 -0700

    Verison 1.0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="django-auth-ldap",
-    version="1.0.8",
+    version="1.0.9",
     description="Django LDAP authentication backend",
     long_description="""This is a Django authentication backend that authenticates against an LDAP service. Configuration can be as simple as a single distinguished name template, but there are many rich configuration options for working with users, groups, and permissions.
     
@@ -12,7 +12,7 @@ This package requires at least Python 2.3, Django 1.0, and python-ldap 2.0. Docu
     """,
     url="http://bitbucket.org/psagers/django-auth-ldap/",
     author="Peter Sagerson",
-    author_email="psagers_pypi@ignorare.net",
+    author_email="psagers.pypi@ignorare.net",
     license="BSD",
     packages=["django_auth_ldap"],
     classifiers=[

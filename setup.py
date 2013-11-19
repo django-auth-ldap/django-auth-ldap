b43ecf39d1b94b87323515f5e07d36e60dbabd07
Peter Sagerson <psagers.hg@ignorare.net>
2013-11-19 12:11:05 -0800

    Version 1.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="django-auth-ldap",
-    version="1.1.6",
+    version="1.1.7",
     description="Django LDAP authentication backend",
     long_description=open('README').read(),
     url="http://bitbucket.org/psagers/django-auth-ldap/",

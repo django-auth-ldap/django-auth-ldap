8dbff730bd225c20a5cb3920b0bbd923880555d6
Peter Sagerson <psagers@ignorare.net>
2017-09-11 11:16:32 -0700

    Version 1.3.0b1

--- django_auth_ldap/__init__.py
+++ django_auth_ldap/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-version = (1, 2, 15)
-version_string = '.'.join(map(str, version))
+version = (1, 3, 0)
+version_qualifier = 'b1'
+version_string = '.'.join(map(str, version)) + version_qualifier

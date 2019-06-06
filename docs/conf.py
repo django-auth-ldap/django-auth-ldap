01e81261acc5ee2ce4b323094de2eb84c59fa47f
Jon Dufresne <jon.dufresne@gmail.com>
2019-06-05 18:38:11 -0700

    Bump version to prepare for the next release

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,9 @@ copyright = "2009, Peter Sagerson"
 author = "Peter Sagerson"
 
 # The short X.Y version
-version = "1.7"
+version = "2.0"
 # The full version, including alpha/beta/rc tags
-release = "1.7.0"
+release = "2.0.0"
 
 
 # -- General configuration ---------------------------------------------------

a19a117fd20daeff04505008c7a3a4813e26769f
Remi <r.fatcheung@gmail.com>
2020-01-28 12:22:33 -0500

    Properly unbind populate_user request
    Also ensure that the populate_user request is properly unbind

--- django_auth_ldap/backend.py
+++ django_auth_ldap/backend.py
@@ -433,6 +433,8 @@ class _LDAPUser:
         except Exception as e:
             logger.warning("{} while authenticating {}".format(e, self._username))
             raise
+            
+        self._get_connection().unbind_s()
 
         return user
 

b584c9fcd08f739ed6c93d495264207b4d093e7a
Jon Dufresne <jon.dufresne@gmail.com>
2020-03-26 10:52:52 -0700

    Bump version for release

--- django_auth_ldap/__init__.py
+++ django_auth_ldap/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (2, 1, 0)
+VERSION = (2, 1, 1)
 __version__ = ".".join(str(i) for i in VERSION)
 
 # Deprecated. Use VERSION and __version__ instead.
--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = "Peter Sagerson"
 # The short X.Y version
 version = "2.1"
 # The full version, including alpha/beta/rc tags
-release = "2.1.0"
+release = "2.1.1"
 
 
 # -- General configuration ---------------------------------------------------

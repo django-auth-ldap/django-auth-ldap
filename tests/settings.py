1f25694735ca884ea5064d7963fcb5c7873fad8c
Brant <brant.watson@propylon.com>
2020-08-11 14:29:47 -0500

    Add cache values to test settings

--- tests/settings.py
+++ tests/settings.py
@@ -5,3 +5,16 @@ INSTALLED_APPS = ("django.contrib.auth", "django.contrib.contenttypes", "tests")
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 
 AUTHENTICATION_BACKENDS = ["django_auth_ldap.backend.LDAPBackend"]
+
+AUTH_LDAP_CACHE = 'default'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    },
+    'ldap': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'django-auth-ldap',
+    }
+}
